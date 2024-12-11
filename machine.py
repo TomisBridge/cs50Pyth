635e784d98d0e889345a25d9773c381bf50dd4e3
Thomas Bridge <tomisbridge@gmail.com>
2024-12-11 15:35:37 -0800

    add machine.py example from course

---
+++ machine.py
@@ -0,0 +1,15 @@
+emotion = "v.v"
+
+def main():
+    global emotion
+    say("is onyone there")
+    emotion = ":D"
+    say("Oh, hi!")
+
+def say(phrase):
+    print(phrase + " " + emotion)
+
+
+main()
+
+
