b657a94cfc11c50f36bb48b5c4d1daa476082786
Thomas Bridge <tomisbridge@gmail.com>
2024-12-11 16:21:27 -0800

    add parity 1 and 2 examples

---
+++ compare.py
@@ -0,0 +1,15 @@
+x = int(input("what's x? "))
+y = int(input("what's y? "))
+
+
+if x > y:
+    print("x > y")
+elif x < y:
+    print("x < y")
+else:
+    print("x = y")
+
+if x != y:
+    print("x does not equal y")
+else:
+    print("x = y")
---
+++ parity.py
@@ -0,0 +1,8 @@
+x = int(input("what's x? "))
+
+if x % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+
